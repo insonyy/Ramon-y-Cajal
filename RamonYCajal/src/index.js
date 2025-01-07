1dfd83dc33ecc011a2f78d3b1011838b85711a73
ines (casa) <yeetyhaaw@gmail.com>
2025-01-07 17:17:29 +0100

    código listo

---
+++ RamonYCajal/src/index.js
@@ -0,0 +1,57 @@
+const {Client, IntentsBitField, Collection, ChannelType} = require('discord.js');
+require('dotenv').config();
+
+const tokens = [
+    process.env.TOKEN_RAMON,
+    process.env.TOKEN_CAJAL
+];
+const clients = [];
+
+for (const token of tokens){
+
+    const client = new Client({
+        intents: [
+            IntentsBitField.Flags.Guilds,
+            IntentsBitField.Flags.GuildMessages,
+            IntentsBitField.Flags.MessageContent,
+        ]
+    });
+    clients.push(client);
+
+    client.on('ready', (c)=>{
+        console.log(`OK ${c.user.tag} is online`);
+        client.user.setActivity(
+            {
+                name: "ramon y cajal"
+            })
+    });
+
+    client.login(token);
+
+    client.on('interactionCreate', async (interaction) => {
+        if (!interaction.isChatInputCommand()) return;
+
+        if (interaction.commandName === "count_100") {
+            try{
+
+                await interaction.deferReply();
+
+                for ( let i = 0; i <= 100; i++) {
+
+                    const botIndex = i % clients.length;
+                    const currentClient = clients[botIndex];
+
+                    const channel = currentClient.channels.cache.get(interaction.channelId);
+                    if (channel) {
+                        await channel.send(`${i}`);
+                    }
+                }
+
+            }catch (error){
+                console.error("Error handling interaction:", error);
+
+            }
+        }
+    })
+}
+
---
+++ RamonYCajal/src/register-comands.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const {REST, Routes, ApplicationCommandOptionType} = require("discord.js");
+
+const commands = [
+
+    {
+        name: "count_100",
+        description: "command that counts to 100 as fast as possible."
+    }
+
+];
+
+const botConfigs = [
+    {
+        token: process.env.TOKEN_RAMON,
+        client: process.env.CLIENT_ID_RAMON,
+        guildId: process.env.GUILD_ID
+    },
+    {
+        token: process.env.TOKEN_CAJAL,
+        client: process.env.CLIENT_ID_CAJAL,
+        guildId: process.env.GUILD_ID
+    }
+];
+
+(async () => {
+    for(const {token, client, guildId} of botConfigs){
+        const rest = new REST({version: "10"}).setToken(token);
+
+        try{
+            console.log(`Registering slash commands for bot with Client ID: ${client}...`);
+
+            await rest.put(
+                Routes.applicationGuildCommands(client, guildId),
+                { body: commands }
+            );
+
+        }catch (error){
+            console.error(`There was an error registering commands for bot with Client ID: ${client}:`, error);
+        }
+    }
+})();
